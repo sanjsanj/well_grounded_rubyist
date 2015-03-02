101c2964a29852f9b6312d3257cbda7de4856c8c
sanjsanj <sanjsanj@hotmail.com>
2015-03-02 10:16:41 +0000

    Complete exercise 1.3.1

---
+++ 1.3.1_loaddemo.rb
@@ -0,0 +1,3 @@
+puts "This is the first (master) program file."
+load "1.3.1_loadee.rb"
+puts "And back again to the first file."
---
+++ 1.3.1_requiredemo.rb
@@ -0,0 +1,3 @@
+puts "This is the first (master) program file."
+require "./1.3.1_loadee"
+puts "And back again to the first file."
