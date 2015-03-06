8ff366c3d5dbf600d4c5c0a1c92c65337c80d91b
sanjsanj <sanjsanj@hotmail.com>
2015-03-06 12:02:29 +0000

    Complete 3.2.1_init_object_with_state

---
+++ 3.2.1_init_object_with_state.rb
@@ -0,0 +1,20 @@
+class Ticket
+  def initialize(venue, date)
+    @venue = venue
+    @date = date
+  end
+  
+  def venue
+    @venue
+  end
+  
+  def date
+    @date
+  end
+end
+
+th = Ticket.new("Town Hall", "11/12/13")
+cc = Ticket.new("Convention Center", "12/13/14")
+
+puts "The first ticket is for a #{th.venue} event on #{th.date}."
+puts "The second is for an event on #{cc.date} at #{cc.venue}."
